16ea11e3e7dd9a77efc4c0032131a5bc389bd87b
Bruno <bruno.ariev@gmail.com>
2019-06-13 15:54:43 -0300

    Impl. Q1Simulation

--- src/main/java/br/senac/sp/probabilidade/question/Q1Simulation.java
+++ src/main/java/br/senac/sp/probabilidade/question/Q1Simulation.java
@@ -1,24 +1,43 @@
 package br.senac.sp.probabilidade.question;
 
-import br.senac.sp.probabilidade.model.Company;
 import br.senac.sp.probabilidade.model.Product;
 import br.senac.sp.probabilidade.model.Sales;
 
-import java.util.ArrayList;
+import java.time.Month;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Q1Simulation implements Simulation {
 
-	private Company company;
-	private List<Sales> sales;
-
-	public Q1Simulation(Company company) {
-		this.company = company;
-		this.sales = new ArrayList<>();
-	}
+	private final static Integer FIRST_MONTH_SALES = 250;
+	private final static Integer THIRD_MONTH_SALES = 320;
 
 	@Override
-	public Simulation runWith(Product... products) {
-		return this;
+	public Collection<Sales> computeSalesOf(Product... products) {
+		Product p1 = products[0];
+		Product p2 = products[1];
+
+		Integer p1SalesInJan = Simulation.rng(FIRST_MONTH_SALES);
+		Sales janSales = Sales.of(Month.JANUARY)
+			.addItem(p1, p1SalesInJan)
+			.addItem(p2, FIRST_MONTH_SALES - p1SalesInJan);
+
+		Sales febSales = Sales.of(Month.FEBRUARY)
+			.addItem(p1, Double.valueOf(janSales.getItems().get(0).getQuantity() * 1.1).intValue())
+			.addItem(p2, Double.valueOf(janSales.getItems().get(1).getQuantity() * 1.1).intValue());
+
+		List<Sales> sales = IntStream.rangeClosed(3, 12).mapToObj(month -> {
+			int p1SalesInMarch = Simulation.rng(THIRD_MONTH_SALES);
+			return Sales.of(Month.of(month))
+				.addItem(p1, p1SalesInMarch)
+				.addItem(p2, THIRD_MONTH_SALES - p1SalesInMarch);
+		}).collect(Collectors.toList());
+
+		sales.add(0, janSales);
+		sales.add(1, febSales);
+		return sales;
 	}
+
 }
