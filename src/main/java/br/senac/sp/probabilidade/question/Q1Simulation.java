97b38284547acee9fa84592b8a998ad61f6fefbe
Bruno <bruno.ariev@gmail.com>
2019-06-13 15:21:38 -0300

    Including questions for Simulation

---
+++ src/main/java/br/senac/sp/probabilidade/question/Q1Simulation.java
@@ -0,0 +1,24 @@
+package br.senac.sp.probabilidade.question;
+
+import br.senac.sp.probabilidade.model.Company;
+import br.senac.sp.probabilidade.model.Product;
+import br.senac.sp.probabilidade.model.Sales;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q1Simulation implements Simulation {
+
+	private Company company;
+	private List<Sales> sales;
+
+	public Q1Simulation(Company company) {
+		this.company = company;
+		this.sales = new ArrayList<>();
+	}
+
+	@Override
+	public Simulation runWith(Product... products) {
+		return this;
+	}
+}
---
+++ src/main/java/br/senac/sp/probabilidade/question/Simulation.java
@@ -0,0 +1,9 @@
+package br.senac.sp.probabilidade.question;
+
+import br.senac.sp.probabilidade.model.Product;
+
+public interface Simulation {
+
+	Simulation runWith(Product... products);
+
+}
