16f5b050485de6214143c82fecbbbe2a8cf95825
Bruno <bruno.ariev@gmail.com>
2019-06-13 17:16:30 -0300

    Scaffolding report

---
+++ src/main/java/br/senac/sp/probabilidade/question/Report.java
@@ -0,0 +1,11 @@
+package br.senac.sp.probabilidade.question;
+
+import br.senac.sp.probabilidade.model.Company;
+
+public class Report {
+
+	public static void generateReport(Company company, String filename) {
+
+	}
+
+}
