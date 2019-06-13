596f507b1730620ce3a84c8c65463edf8da9916e
Bruno <bruno.ariev@gmail.com>
2019-06-13 16:13:31 -0300

    Impl. Q2 Simulation

--- src/main/java/br/senac/sp/probabilidade/question/Q2Simulation.java
+++ src/main/java/br/senac/sp/probabilidade/question/Q2Simulation.java
@@ -1,24 +1,43 @@
 package br.senac.sp.probabilidade.question;
 
-import br.senac.sp.probabilidade.model.Company;
 import br.senac.sp.probabilidade.model.Product;
 import br.senac.sp.probabilidade.model.Sales;
 
+import java.time.Month;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class Q2Simulation implements Simulation {
 
-	private Company company;
-	private List<Sales> sales;
-
-	public Q2Simulation(Company company) {
-		this.company = company;
-		this.sales = new ArrayList<>();
-	}
+	private final static Integer FIRST_MONTH_SALES = 300;
 
 	@Override
-	public Simulation runWith(Product... products) {
-		return this;
+	public Collection<Sales> computeSalesOf(Product... products) {
+		Product p1 = products[0];
+		Product p2 = products[1];
+
+		Integer totalSales = FIRST_MONTH_SALES;
+		List<Sales> sales = new ArrayList<>();
+
+		for (Month month : Month.values()) {
+			Integer p1Sales = Simulation.rng(totalSales);
+			sales.add(Sales.of(month)
+				.addItem(p1, p1Sales)
+				.addItem(p2, totalSales - p1Sales));
+			totalSales = salesVariance(totalSales);
+		}
+
+		return sales;
 	}
+
+	private Integer salesVariance(Integer totalSales) {
+		Integer rng = Simulation.rng(100);
+		if (rng >= 0 && rng <= 50) {
+			return Math.round(totalSales * 1.1f);
+		}
+		return Math.round(totalSales * 0.9f);
+	}
+
+
 }
