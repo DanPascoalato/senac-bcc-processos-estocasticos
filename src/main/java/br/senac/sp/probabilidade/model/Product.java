e131aee4f8e8209fd5f5b531cfd77e2a8ae532ac
Bruno <bruno.ariev@gmail.com>
2019-06-13 13:05:15 -0300

    Refactoring Product with Builder pattern and costSubtotal

--- src/main/java/br/senac/sp/probabilidade/model/Product.java
+++ src/main/java/br/senac/sp/probabilidade/model/Product.java
@@ -1,21 +1,57 @@
 package br.senac.sp.probabilidade.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Product {
 
-	private Integer qttyA;
-	private Integer qttyB;
+	private List<Component> components;
+	private Double unitPrice;
 
-	public Product(Integer qttyA, Integer qttyB) {
-		this.qttyA = qttyA;
-		this.qttyB = qttyB;
+	private Product(List<Component> components, Double unitPrice) {
+		this.components = components;
+		this.unitPrice = unitPrice;
 	}
 
-	public Integer getQttyA() {
-		return qttyA;
+	public Double getProductionCost() {
+		return components.stream()
+			.map(Component::getProductionCost)
+			.reduce(0.0, (a,b) -> a+b);
 	}
 
-	public Integer getQttyB() {
-		return qttyB;
+
+	public List<Component> getComponents() {
+		return components;
+	}
+
+	public Double getUnitPrice() {
+		return unitPrice;
+	}
+
+	public static Builder builder() {
+		return new Builder();
+	}
+
+	public static class Builder {
+
+		private List<Component> components = new ArrayList<>();
+		private Double unitPrice;
+
+		public Builder component(Component component, Integer qtty) {
+			this.components.addAll(Collections.nCopies(qtty, component));
+			return this;
+		}
+
+		public Builder unitPrice(Double unitPrice) {
+			this.unitPrice = unitPrice;
+			return this;
+		}
+
+		public Product build() {
+			return new Product(components, unitPrice);
+		}
+
 	}
 
 }
