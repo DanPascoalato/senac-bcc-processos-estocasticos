24f00f8c34cede1d5ec5802a8f161f6efbad2a97
Bruno <bruno.ariev@gmail.com>
2019-06-13 18:43:17 -0300

    Impl. report

--- src/main/java/br/senac/sp/probabilidade/question/Report.java
+++ src/main/java/br/senac/sp/probabilidade/question/Report.java
@@ -14,16 +14,70 @@ public class Report {
 		Path filepath = Paths.get("reports/" + filename);
 		Files.createDirectories(filepath.getParent());
 		Files.write(filepath, buildHeader(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+		Files.write(filepath, buildData(company), StandardOpenOption.APPEND);
 	}
 
 	private static byte[] buildHeader() {
+		return new StringBuilder()
+			.append("# Month;")
+
+			.append("P1 (units_sold)").append(";")
+			.append("P1 (unit_price)").append(";")
+			.append("P1 (revenue_subtotal)").append(";")
+			.append("P1 (unit_cost)").append(";")
+			.append("P1 (cost_subtotal)").append(";")
+			.append("P1 (profit_subtotal)").append(";")
+
+			.append("P2 (units_sold)").append(";")
+			.append("P2 (unit_price)").append(";")
+			.append("P2 (revenue_subtotal)").append(";")
+			.append("P2 (unit_cost)").append(";")
+			.append("P2 (cost_subtotal)").append(";")
+			.append("P2 (profit_subtotal)").append(";")
+
+			.append("Total Units Sold").append(";")
+			.append("Total Revenue").append(";")
+			.append("Total Cost").append(";")
+			.append("Total Profit").append(";")
+
+			.append("Payroll").append(";")
+			.append("Other Expenses").append(";")
+
+			.append("Overall Costs").append(";")
+			.append("Overall Profit").append("\n")
+			.toString().getBytes();
+	}
+
+	public Report() {
+	}
+
+	private static byte[] buildData(Company company) {
 		StringBuilder sb = new StringBuilder();
-		sb.append("# Month;");
-		sb.append("P1 (unit_sales); P1 (revenue); P1 (production_cost); P1 (profit);");
-		sb.append("P2 (unit_sales); P2 (revenue); P2 (production_cost);  P2 (profit);");
-		sb.append("Sales Revenue; Sales Prod. Cost; Sales Profit");
-		sb.append("Payroll; Other Expenses");
-		sb.append("\n");
+
+		company.getSales().forEach(sales -> {
+			sb.append(sales.getMonth()).append(";");
+
+			sales.getItems().forEach(item ->
+				sb.append(item.getQuantity()).append(";")
+					.append(item.getProduct().getUnitPrice()).append(";")
+					.append(item.getRevenueSubtotal()).append(";")
+					.append(item.getProduct().getProductionCost()).append(";")
+					.append(item.getCostSubtotal()).append(";")
+					.append(item.getProfitSubtotal()).append(";")
+			);
+
+			sb.append(sales.getUnitsSold()).append(";")
+				.append(sales.getRevenueSubtotal()).append(";")
+				.append(sales.getCostSubtotal()).append(";")
+				.append(sales.getProfitSubtotal()).append(";");
+
+			sb.append(company.getPayroll().getSubtotal()).append(";")
+				.append(company.getExpenses().getSubtotal()).append(";");
+
+			sb.append(company.getTotalCosts()).append(";")
+				.append(company.getTotalProfit()).append("\n");
+		});
+
 		return sb.toString().getBytes();
 	}
 
