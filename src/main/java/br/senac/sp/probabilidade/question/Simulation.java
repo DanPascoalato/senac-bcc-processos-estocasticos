27fae2b398ed43f5581bab39c2c6687de6d9a7d3
Bruno <bruno.ariev@gmail.com>
2019-06-13 15:33:59 -0300

    Incl. rng on Simulation

--- src/main/java/br/senac/sp/probabilidade/question/Simulation.java
+++ src/main/java/br/senac/sp/probabilidade/question/Simulation.java
@@ -2,8 +2,14 @@ package br.senac.sp.probabilidade.question;
 
 import br.senac.sp.probabilidade.model.Product;
 
+import java.util.Random;
+
 public interface Simulation {
 
 	Simulation runWith(Product... products);
+
+	static Integer rng(Integer maxLimit) {
+		return new Random().nextInt(maxLimit+1);
+	}
 
 }
